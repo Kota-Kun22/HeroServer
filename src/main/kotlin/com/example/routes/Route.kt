fe6d9ccf359f0744242caaead4928e671987c7c1
Harsh Rastogi <68362699+Kota-Kun22@users.noreply.github.com>
2024-12-01 11:43:17 +0530

    creating the root endpoint Route.kt file and an extension function

---
+++ src/main/kotlin/com/example/routes/Route.kt
@@ -0,0 +1,17 @@
+package com.example.routes
+
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
+
+fun Route.root() {
+
+    get("/"){
+        call.respond(
+            message = "Welcome to my Hero API !!! ",
+            status = io.ktor.http.HttpStatusCode.OK
+        )
+
+    }
+
+}
