5729570656397a97b93644d0aa3ad8ea63964a21
Harsh Rastogi <68362699+Kota-Kun22@users.noreply.github.com>
2024-12-11 23:47:29 +0530

    documentation added

--- src/main/kotlin/com/example/plugins/DefaultHandler.kt
+++ src/main/kotlin/com/example/plugins/DefaultHandler.kt
@@ -7,6 +7,14 @@ import io.ktor.server.plugins.defaultheaders.DefaultHeaders
 import java.time.Duration
 
 
+/**
+ * Configures default headers for the Ktor application.
+ *
+ * This method installs the `DefaultHeaders` plugin and adds a `Cache-Control` header with a
+ * max age of one year and the `immutable=true` directive. This setup helps optimize caching
+ * behavior by allowing clients to reuse responses and minimize repeated requests, which can
+ * improve application performance and reduce server load.
+ */
 fun Application.configureDefaultHeader()
 {
     install(DefaultHeaders){
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -9,6 +9,20 @@ import io.ktor.server.http.content.static
 
 import io.ktor.server.routing.*
 
+/**
+ * Configures the routing for the Ktor application.
+ *
+ * This function sets up several API endpoints for handling HTTP requests and serving responses.
+ * The following routes are defined:
+ *
+ * - `root`: The default endpoint, which welcomes the user to the Hero API.
+ * - `getAllHeroes`: Handles requests to fetch a paginated list of heroes.
+ * - `searchHeroes`: Allows searching heroes by name.
+ * - Static resources: Serves images stored in the "images" directory under the `/images` path.
+ *
+ * The function takes advantage of Ktor's routing capabilities to define and handle these routes,
+ * enabling the application to serve dynamic and static content effectively.
+ */
 fun Application.configureRouting() {
     routing {
         root()//first default end point
